1a801bf014e1e2f923f41eeaeba0bd11546918e8
scott198989 <youremail@example.comscott-tuschl@comcast.net>
2023-05-08 16:57:44 -0500

    commit by st

--- src/components/Square.js
+++ src/components/Square.js
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Square = (props) => {
   return (
-    <div className="square"></div>
-  )
+  <>
+  <div id="grid">
+   {props.squares.map((value,index)=> {
+    return(
+      <div className="square" key={index}>
+      {value}
+      </div>
+    )
+   })}
+</div>
+</>
+)
 }
+  
+
 export default Square
