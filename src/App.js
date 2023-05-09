b39746eee6cce61428f08f4c158baed38ea6d430
RodCato <code@eliment.org>
2023-05-09 14:04:25 -0500

    update to code for user click

--- src/App.js
+++ src/App.js
@@ -3,30 +3,43 @@ import Square from './components/Square'
 import './App.css'
 
 const App = () => {
-  const [squares, setSquares] = useState(Array(9).fill(null))
+  const [squares, setSquares] = useState(Array(9).fill(""))
+  const [go, setGo] = useState("circle")
+  
+  const message = "it is now" + go + " 's go."
+      return (
+        <div className="app">
+          <div id="grid">
+            {squares.map((square, index)=>
+            <Square
+                key={index}
+                id={index}
+                squares={squares}
+                setSquares={setSquares}
+                go={go}
+                setGo={setGo}
+                square={square}
+              />)}
+          </div>
+        </div>
 
-  handleClick('i'); {
-    const board = this.state.board.slice();
-    board['i'] = this.state.currentPlayer;
-    this.setState({
-      board: board,
-      currentPlayer: this.state.currentPlayer === "X" ? "O" : "X",
-    });
-  }
-
-const handleClick = (selectedIndex) => {
-  //alert(selectedIndex)
+      )
 
 
-  const gridUpdate = [...squares];
-  gridUpdate [selectedIndex] ="🖕";
-  setSquares(gridUpdate);
-}
 
   return (
     <>
+
       <h1>Tic Tac Toe</h1>
-      <Square squares={squares} handleClick={handleClick}/>
+      <div className="square">
+       <div id="grid" >{squares.map((squares, index) => <Square 
+                                             key={index} 
+                                             id={index} 
+                                             squares={squares}
+                                            /> )}
+                                             </div>
+        </div>
+      
       
     </>
   )
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,34 +1,30 @@
 import React from 'react'
 
-const Square = (props) => {
-  console.log(props.squares)
+const Square = ({id, go, square}) => {
+  
+  const handleClick = (s) => {
+    const taken = s.target.firstChild.classList.contains("circle") || 
+    s.target.firstChild.classList.contains("cross")
+  
+    if(!taken){
+        if(go === "circle"){
+          s.target.firstChild.classList.add("circle")
+        }
+        if(go === "cross"){
+          s.target.firstChild.classList.add("cross")
+        }
+    }  
+  }
   return (
   <>
-  render() {
- (
-    <div className="board">
-      {this.state.board.map((cell, i) => (
-        <div className="cell" key={i} onClick={() => this.handleClick(i)}>
-          {cell}
-        </div>
-      ))}
-    </div>
-  )
-}
-  <div id="grid" >
-   {props.squares.map((value,index)=> {
-    return(
-      <div className="square" 
-      key={index}
-      onClick={() => {props.handleClick(index)}}>
-      {value}
-      </div>
-    )
-   })}
-</div>
+ <div className="square" id={id} onClick={handleClick}>
+ <div className={square}></div>
+ </div>
+
 </>
 )
 }
+
   
 
 export default Square
