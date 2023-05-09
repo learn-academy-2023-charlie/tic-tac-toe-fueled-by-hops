ccc4954c1ba730654e271d7ed190ea87a8eb4035
scott198989 <youremail@example.comscott-tuschl@comcast.net>
2023-05-09 11:48:32 -0500

    st commit

--- src/App.js
+++ src/App.js
@@ -3,33 +3,14 @@ import Square from './components/Square'
 import './App.css'
 
 const App = () => {
-  const [squares, setSquares] = useState(Array(9).fill(null))
+return (
+<div className= "app">
+  <div className="Tic-Tac-Toe">
+  <Square />
+  </div>
 
-  handleClick('i'); {
-    const board = this.state.board.slice();
-    board['i'] = this.state.currentPlayer;
-    this.setState({
-      board: board,
-      currentPlayer: this.state.currentPlayer === "X" ? "O" : "X",
-    });
-  }
-
-const handleClick = (selectedIndex) => {
-  //alert(selectedIndex)
-
-
-  const gridUpdate = [...squares];
-  gridUpdate [selectedIndex] ="🖕";
-  setSquares(gridUpdate);
+  <p></p>
+</div>
+)
 }
-
-  return (
-    <>
-      <h1>Tic Tac Toe</h1>
-      <Square squares={squares} handleClick={handleClick}/>
-      
-    </>
-  )
-}
-
 export default App
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,34 +1,9 @@
 import React from 'react'
 
-const Square = (props) => {
-  console.log(props.squares)
-  return (
-  <>
-  render() {
- (
-    <div className="board">
-      {this.state.board.map((cell, i) => (
-        <div className="cell" key={i} onClick={() => this.handleClick(i)}>
-          {cell}
-        </div>
-      ))}
-    </div>
-  )
-}
-  <div id="grid" >
-   {props.squares.map((value,index)=> {
-    return(
-      <div className="square" 
-      key={index}
-      onClick={() => {props.handleClick(index)}}>
-      {value}
-      </div>
+const Square = () => {
+return (
+    <div className="square"></div>
     )
-   })}
-</div>
-</>
-)
 }
-  
 
 export default Square
