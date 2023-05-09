cb1fc1bd7d3cec15727a3b8bd87e481857d2b190
RodCato <code@eliment.org>
2023-05-09 15:07:57 -0500

    added players turn functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Square from './components/Square'
 import './App.css'
 
 const App = () => {
   const [squares, setSquares] = useState(Array(9).fill(""))
   const [go, setGo] = useState("circle")
+  const [winningMessage, setWinningMessage] = useState(null)
   
-  const message = "it is now" + go + " 's go."
+  const message = "It is now " + go + "'s go."
+  console.log(squares)
+
+  const checkScore = () => {
+    const winningCombos = [
+      [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]
+    ]
+    winningCombos.forEach(array => {
+      let circleWin = array.every(square => squares[square] === "circle")
+       if (circleWin){
+        setWinningMessage("Circle Wins!")
+        return
+       }
+    })
+
+    winningCombos.forEach(array => {
+      let crossWin = array.every(square => squares[square] === "cross")
+       if (crossWin){
+        setWinningMessage("Cross Wins!")
+        return
+       }
+    })
+
+  }
+
+  useEffect(() => {
+    checkScore()
+  }, [squares])
+
       return (
         <div className="app">
           <div id="grid">
@@ -19,8 +48,10 @@ const App = () => {
                 go={go}
                 setGo={setGo}
                 square={square}
+                winningMessage={winningMessage}
               />)}
           </div>
+          <p>{winningMessage || message}</p>
         </div>
 
       )
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,30 +1,42 @@
 import React from 'react'
 
-const Square = ({id, go, square}) => {
-  
-  const handleClick = (s) => {
-    const taken = s.target.firstChild.classList.contains("circle") || 
-    s.target.firstChild.classList.contains("cross")
-  
-    if(!taken){
+const Square = ({id, go, setGo, square, squares, setSquares, winningMessage}) => {
+ 
+  const handleClick = (e) => {
+    const taken = e.target.firstChild.classList.contains("circle") || 
+    e.target.firstChild.classList.contains("cross")
+      if(!taken){
         if(go === "circle"){
-          s.target.firstChild.classList.add("circle")
+          e.target.firstChild.classList.add("circle")
+          handleSquareChange("circle")
+          setGo("cross")
         }
         if(go === "cross"){
-          s.target.firstChild.classList.add("cross")
+          e.target.firstChild.classList.add("cross")
+          handleSquareChange("cross")
+          setGo("circle")
         }
     }  
   }
+
+  const handleSquareChange = (className) =>{
+     const nextSquares = squares.map((square, index) => {
+        if  (index === id){
+          return className
+        } else{
+          return square
+        }
+      })
+      setSquares(nextSquares)
+  }
   return (
   <>
- <div className="square" id={id} onClick={handleClick}>
+ <div className="square" id={id} onClick={!winningMessage && handleClick}>
  <div className={square}></div>
  </div>
 
 </>
 )
 }
-
-  
 
 export default Square
